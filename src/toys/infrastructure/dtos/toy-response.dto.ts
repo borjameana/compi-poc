2d100b2cc530bfe520e01a6919bda3bfd32da6e9
Borja Meana Gonzalez <borja.meana@the-cocktail.com>
2026-06-02 08:30:48 +0200

    Compi: automated task

--- src/toys/infrastructure/dtos/toy-response.dto.ts
+++ src/toys/infrastructure/dtos/toy-response.dto.ts
@@ -1,11 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { ToyEntity } from '../../domain/entities/toy.entity';
 
 export class ToyResponseDto {
+    @ApiProperty({ description: 'The unique identifier of the toy', example: '1a2b3c4d5e6f7a8b9c0d1e2f' })
     id!: string;
+
+    @ApiProperty({ description: 'The price of the toy', example: 25.99 })
     price!: number;
+
+    @ApiProperty({ description: 'The brand of the toy', example: 'Lego' })
     brand!: string;
+
+    @ApiProperty({ description: 'The model of the toy', example: 'Castle' })
     model!: string;
+
+    @ApiProperty({ description: 'The color of the toy', example: 'red' })
     color!: string;
+
+    @ApiProperty({ description: 'The recommended age for the toy', example: 8 })
     age!: number;
 
     static from(entity: ToyEntity): ToyResponseDto {
--- src/users/infrastructure/dtos/user-response.dto.ts
+++ src/users/infrastructure/dtos/user-response.dto.ts
@@ -1,10 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { UserEntity } from '../../domain/entities/user.entity';
 
 export class UserResponseDto {
+    @ApiProperty({ description: 'The unique identifier of the user', example: '11223344-5566-7788-9900-aabbccddeeff' })
     id!: string;
+
+    @ApiProperty({ description: 'The name of the user', example: 'John' })
     name!: string;
+
+    @ApiProperty({ description: 'The surname of the user', example: 'Doe' })
     surname!: string;
+
+    @ApiProperty({ description: 'The age of the user', example: 30 })
     age!: number;
+
+    @ApiProperty({ description: 'The country of residence of the user', example: 'USA' })
     country!: string;
 
     static from(entity: UserEntity): UserResponseDto {
